2d781bd7686133c30f221ff86d5d18d0900d307b
Kyle Zaw <zaw@prp.bio.purdue.edu>
2020-07-10 02:14:26 -0400

    GUI2 bug fixes

--- GUI2/main.py
+++ GUI2/main.py
@@ -12,54 +12,38 @@ from file_crawler import parse_relion_project, parse_csparc_project
 from file_crawler import parse_particles_cryoem_projects
 
 class entryItem(QStandardItem):
-	def __init(self, txt='', font_size=16, set_bold=True, color=QColor(255, 255, 255)):
-		super().__init__()
+        def __init(self, txt='', font_size=16, set_bold=True, color=QColor(255, 255, 255)):
+                super().__init__()
 
-		fnt = QFont('Open Sans', font_size)
-		fnt.setBold(set_bold)
+                fnt = QFont('Open Sans', font_size)
+                fnt.setBold(set_bold)
 
-		self.setEditable(False)
-		self.setForeground(color)
-		self.setFont(fnt)
-		self.setText(txt)
+                self.setEditable(False)
+                self.setForeground(color)
+                self.setFont(fnt)
+                self.setText(txt)
 
 class subEntryItem(QStandardItem):
-	def __init__(self, txt='', font_size=14, set_bold=False, color=QColor(255, 255, 255)):
-		super().__init__()
+        def __init__(self, txt='', font_size=14, set_bold=False, color=QColor(255, 255, 255)):
+                super().__init__()
 
-		fnt = QFont('Open Sans', font_size)
-		fnt.setBold(set_bold)
+                fnt = QFont('Open Sans', font_size)
+                fnt.setBold(set_bold)
 
-		self.setEditable(False)
-		self.setForeground(color)
-		self.setFont(fnt)
-		self.setText(txt)
+                self.setEditable(False)
+                self.setForeground(color)
+                self.setFont(fnt)
+                self.setText(txt)
 
 class dbModel(QStandardItemModel):
-	def __init__(self, *args, entries=None, **kwargs):
-		super(dbModel, self).__init__(*args, **kwargs)
-		if entries:
-			self.entries = entries
-		else:
-			self.entries = []
+        def __init__(self, *args, entries=None, **kwargs):
+                super(dbModel, self).__init__(*args, **kwargs)
+                if entries:
+                        self.entries = entries
+                else:
+                        self.entries = []
 
-		self.parentItem = self.invisibleRootItem()
-		# Entries are stored as a list of lists, where each sublist
-		# has its first element as the entry name and the following
-		# elements as dictionaries containing keys for 'job_type', voltage', 'cs'
-		# 'amp_cont', 'psize', 'num_mics', 'num_particles' and 'location'
-		if entries:
-			for entry in entries:
-				main_entry = entry[0]
-				sub_entries = entry[1:]
-
-				entry_node = entryItem(main_entry)
-				for sub_entry in sub_entries:
-					sub_entry_node = subEntryItem('%s %s' % (sub_entry['job_type']), sub_entry['job_size'])
-					entry_node.append(sub_entry_node)
-
-				self.parentItem.append(entry_node)
-
+                
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
@@ -69,79 +53,103 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.parserButton.clicked.connect(self.auto_parser)
         self.nnButton.clicked.connect(self.train_nn)
         self.pickButton.clicked.connect(self.pick_particles)
+
         self.dbModel = dbModel()
         self.update_dbModel()
-        self.dbTreeView = QTreeView()
-        self.dbTreeView.setModel(self.dbModel)
+        self.treeView.setModel(self.dbModel)
+        self.treeView.clicked.connect(self.update_metadata_info)
 
     def import_relion_project(self):
-	    fname = QFileDialog.getExistingDirectory(self, 
-	    	'Select a Relion project folder')
-	    if fname:
-	    	text, ok = QInputDialog.getText(self, 'Entry Name',
-	    		'Enter name of entry')
-	    	if text and ok:
-	    		entry_name = text
-	    		parse_relion_project(fname, entry_name)
-	    		self.update_dbModel()
+            fname = QFileDialog.getExistingDirectory(self, 
+                'Select a Relion project folder')
+            if fname:
+                text, ok = QInputDialog.getText(self, 'Entry Name',
+                        'Enter name of entry')
+                if text and ok:
+                        entry_name = text
+                        parse_relion_project(fname, entry_name)
+                        self.update_dbModel()
 
     def import_csparc_project(self):
-    	fname = QFileDialog.getExistingDirectory(self,
-    		'Select a CSparc project folder')
-    	if fname:
-    		text, ok = QInputDialog.getText(self, 'Entry Name',
-    			'Enter name of entry')
-    		if text and ok:
-    			entry_name = text
-    			parse_csparc_project(fname, entry_name)
-    			self.update_dbModel()
+        fname = QFileDialog.getExistingDirectory(self,
+                'Select a CSparc project folder')
+        if fname:
+                text, ok = QInputDialog.getText(self, 'Entry Name',
+                        'Enter name of entry')
+                if text and ok:
+                        entry_name = text
+                        parse_csparc_project(fname, entry_name)
+                        self.update_dbModel()
 
     def auto_parser(self):
-    	dir_name = QFileDialog.getExistingDirectory(self,
-    		'Select the root folder')
-    	if dir_name:
-    		parse_particles_cryoem_projects(dir_name)
+        dir_name = QFileDialog.getExistingDirectory(self,
+                'Select the root folder')
+        if dir_name:
+                parse_particles_cryoem_projects(dir_name)
 
     def train_nn(self):
-    	pass
+        pass
 
     def pick_particles(self):
-    	pass
+        pass
 
     def update_dbModel(self):
-    	self.dbModel.entries = []
-    	entries = [os.path.join('../Database', f) for f in os.listdir('../Database') if
-    		os.path.isdir(os.path.join('../Database/', f))]
-    	for entry in entries:
-    		entryList = []
-    		entryName = os.path.basename(os.path.normpath(entry))
-    		entryList.append('%s %s' % (entryName, self.get_size_format(self.get_directory_size(entry))))
+        self.dbModel.entries = []
+        entries = [os.path.join('../Database', f) for f in os.listdir('../Database') if
+                os.path.isdir(os.path.join('../Database/', f))]
 
-    		subEntries = [os.path.join(entry, f) for f in os.listdir(entry) if os.path.isdir(os.path.join(entry, f))]
-    		for i in range(0, len(subEntries)):
-    			entryList.append({})
-    			with open(os.path.join(subEntries[i], 'info.txt'), 'r') as info_f:
-    				num_particles = info_f.readline().split()[-1]
-    				num_mics = info_f.readline().split()[-1]
-    				voltage = info_f.readline().split()[-1]
-    				cs = info_f.readline().split()[-1]
-    				amp_cont = info_f.readlin().split()[-1]
-    				psize = info_f.readline().split()[-1]
+        for entry in entries:
+                entryList = []
+                entryName = os.path.basename(os.path.normpath(entry))
+                entryList.append('%s %s' % (entryName, self.get_size_format(self.get_directory_size(entry))))
+                
+                particles_path = os.path.join(entry, 'Particles')
+                subEntries = [os.path.join(particles_path, f) for f in os.listdir(particles_path) if os.path.isdir(os.path.join(particles_path, f))]
+                for i in range(0, len(subEntries)):
+                        entryList.append({})
+                        with open(os.path.join(subEntries[i], 'info.txt'), 'r') as info_f:
+                                num_particles = info_f.readline().split()[-1]
+                                num_mics = info_f.readline().split()[-1]
+                                voltage = info_f.readline().split()[-1]
+                                cs = info_f.readline().split()[-1]
+                                amp_cont = info_f.readline().split()[-1]
+                                psize = info_f.readline().split()[-1]
 
-    				entry_List[i+1]['job_type'] = os.path.split(subEntries[i])[-1]
-    				entry_List[i+1]['num_particles'] = num_particles
-    				entry_list[i+1]['num_mics'] = num_mics
-    				entry_list[i+1]['voltage'] = voltage
-    				entry_list[i+1]['cs'] = cs
-    				entry_list[i+1]['amp_cont'] = amp_cont
-    				entry_list[i+1]['psize'] = psize
-    				entry_list[i+1]['job_size'] = self.get_size_format(self.get_directory_size(entry))
-    		self.dbModel.entries.append(entryList)
+                                entryList[i+1]['job_type'] = os.path.split(subEntries[i])[-1]
+                                entryList[i+1]['num_particles'] = num_particles
+                                entryList[i+1]['num_mics'] = num_mics
+                                entryList[i+1]['voltage'] = voltage
+                                entryList[i+1]['cs'] = cs
+                                entryList[i+1]['amp_cont'] = amp_cont
+                                entryList[i+1]['psize'] = psize
+                                entryList[i+1]['job_size'] = self.get_size_format(self.get_directory_size(subEntries[i]))
+                self.dbModel.entries.append(entryList)
 
-    	self.dbModel.layoutChanged.emit()
+        rootItem = self.dbModel.invisibleRootItem()
+        # Entries are stored as a list of lists, where each sublist
+        # has its first element as the entry name and the following
+        # elements as dictionaries containing keys for 'job_type', voltage', 'cs'
+        # 'amp_cont', 'psize', 'num_mics', 'num_particles' and 'location'
+        entries = self.dbModel.entries
+        if entries:
+                for entry in entries:
+                        main_entry = entry[0]
+                        sub_entries = entry[1:]
 
-    def update_metada_info(self):
-    	pass
+                        entry_node = entryItem(main_entry)
+                        for sub_entry in sub_entries:
+                                sub_entry_node = subEntryItem('%s %s' % (sub_entry['job_type'], sub_entry['job_size']))
+                                print('%s %s' % (sub_entry['job_type'], sub_entry['job_size']))
+                                entry_node.appendRow(sub_entry_node)
+
+                        rootItem.appendRow(entry_node)
+
+
+        self.dbModel.layoutChanged.emit()
+
+    def update_metadata_info(self):
+        index = self.treeView.currentIndex()
+        print(index)
 
     def get_directory_size(self, dir_path):
         total = 0
@@ -168,10 +176,10 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         return f"{num_bytes:.2f}Y{suffix}"
 
 def main():
-	app = QApplication(sys.argv)
-	window = MainWindow()
-	window.show()
-	sys.exit(app.exec_())
+        app = QApplication(sys.argv)
+        window = MainWindow()
+        window.show()
+        sys.exit(app.exec_())
 
 if __name__ == '__main__':
-	main()
+        main()
