b916e2bbf0a827644ec8ac7896332ebc3f5d2c53
Kyle Zaw <zaw@prp.bio.purdue.edu>
2020-07-13 16:17:49 -0400

    cryolo pipeline work and test files

---
+++ Scripts/cryolo_pipeline.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+""" Cryolo Pipeline Script
+
+Contains functions that handle the pipelining and processing of particle data
+from the output of file_crawler.py, to a form usable by the Cryolo software
+for both training and picking.
+
+Can also be used as a standalone executable script.
+Example usage: $ 
+"""
+
+import os
+import subprocess
+
+def main():
+    
+    
+
+if __name__ == '__main__':
+    main()
+
